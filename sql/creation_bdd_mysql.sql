d89c6fa3005783a99c7735cfeaa7b560358f86bb
eltar14 <lebouant2@gmail.com>
2024-06-24 23:59:05 +0200

    sql creation_bdd_mysql.sql

--- sql/creation_bdd_mysql.sql
+++ sql/creation_bdd_mysql.sql
@@ -21,9 +21,9 @@ DROP TABLE IF EXISTS feuillage;
 #------------------------------------------------------------
 
 CREATE TABLE secteur(
-        id_secteur  Int  Auto_increment  NOT NULL ,
-        clc_secteur Varchar (240) NOT NULL
-	,CONSTRAINT secteur_PK PRIMARY KEY (id_secteur)
+                        id_secteur  Int  Auto_increment  NOT NULL ,
+                        clc_secteur Varchar (240) NOT NULL
+    ,CONSTRAINT secteur_PK PRIMARY KEY (id_secteur)
 )ENGINE=InnoDB;
 
 
@@ -32,12 +32,12 @@ CREATE TABLE secteur(
 #------------------------------------------------------------
 
 CREATE TABLE quartier(
-        id_quartier  Int  Auto_increment  NOT NULL ,
-        clc_quartier Varchar (240) NOT NULL ,
-        id_secteur   Int NOT NULL
-	,CONSTRAINT quartier_PK PRIMARY KEY (id_quartier)
+                         id_quartier  Int  Auto_increment  NOT NULL ,
+                         clc_quartier Varchar (240) NOT NULL ,
+                         id_secteur   Int NOT NULL
+    ,CONSTRAINT quartier_PK PRIMARY KEY (id_quartier)
 
-	,CONSTRAINT quartier_secteur_FK FOREIGN KEY (id_secteur) REFERENCES secteur(id_secteur)
+    ,CONSTRAINT quartier_secteur_FK FOREIGN KEY (id_secteur) REFERENCES secteur(id_secteur)
 )ENGINE=InnoDB;
 
 
@@ -46,9 +46,9 @@ CREATE TABLE quartier(
 #------------------------------------------------------------
 
 CREATE TABLE etat(
-        id_etat  Int  Auto_increment  NOT NULL ,
-        arb_etat Varchar (80) NOT NULL
-	,CONSTRAINT etat_PK PRIMARY KEY (id_etat)
+                     id_etat  Int  Auto_increment  NOT NULL ,
+                     arb_etat Varchar (80) NOT NULL
+    ,CONSTRAINT etat_PK PRIMARY KEY (id_etat)
 )ENGINE=InnoDB;
 
 
@@ -57,9 +57,9 @@ CREATE TABLE etat(
 #------------------------------------------------------------
 
 CREATE TABLE stadedev(
-        id_stadedev Int  Auto_increment  NOT NULL ,
-        stadedev    Varchar (80) NOT NULL
-	,CONSTRAINT stadedev_PK PRIMARY KEY (id_stadedev)
+                         id_stadedev Int  Auto_increment  NOT NULL ,
+                         stadedev    Varchar (80) NOT NULL
+    ,CONSTRAINT stadedev_PK PRIMARY KEY (id_stadedev)
 )ENGINE=InnoDB;
 
 
@@ -68,9 +68,9 @@ CREATE TABLE stadedev(
 #------------------------------------------------------------
 
 CREATE TABLE port(
-        id_port Int  Auto_increment  NOT NULL ,
-        port    Varchar (80) NOT NULL
-	,CONSTRAINT port_PK PRIMARY KEY (id_port)
+                     id_port Int  Auto_increment  NOT NULL ,
+                     port    Varchar (80) NOT NULL
+    ,CONSTRAINT port_PK PRIMARY KEY (id_port)
 )ENGINE=InnoDB;
 
 
@@ -79,9 +79,9 @@ CREATE TABLE port(
 #------------------------------------------------------------
 
 CREATE TABLE pied(
-        id_pied Int  Auto_increment  NOT NULL ,
-        pied    Varchar (80) NOT NULL
-	,CONSTRAINT pied_PK PRIMARY KEY (id_pied)
+                     id_pied Int  Auto_increment  NOT NULL ,
+                     pied    Varchar (80) NOT NULL
+    ,CONSTRAINT pied_PK PRIMARY KEY (id_pied)
 )ENGINE=InnoDB;
 
 
@@ -90,9 +90,9 @@ CREATE TABLE pied(
 #------------------------------------------------------------
 
 CREATE TABLE situation(
-        id_situation Int  Auto_increment  NOT NULL ,
-        situaton     Varchar (80) NOT NULL
-	,CONSTRAINT situation_PK PRIMARY KEY (id_situation)
+                          id_situation Int  Auto_increment  NOT NULL ,
+                          situaton     Varchar (80) NOT NULL
+    ,CONSTRAINT situation_PK PRIMARY KEY (id_situation)
 )ENGINE=InnoDB;
 
 
@@ -101,9 +101,9 @@ CREATE TABLE situation(
 #------------------------------------------------------------
 
 CREATE TABLE nomtech(
-        id_nomtech Int  Auto_increment  NOT NULL ,
-        nomtech    Varchar (80) NOT NULL
-	,CONSTRAINT nomtech_PK PRIMARY KEY (id_nomtech)
+                        id_nomtech Int  Auto_increment  NOT NULL ,
+                        nomtech    Varchar (80) NOT NULL
+    ,CONSTRAINT nomtech_PK PRIMARY KEY (id_nomtech)
 )ENGINE=InnoDB;
 
 
@@ -112,9 +112,9 @@ CREATE TABLE nomtech(
 #------------------------------------------------------------
 
 CREATE TABLE villeca(
-        id_villeca Int  Auto_increment  NOT NULL ,
-        villeca    Varchar (80) NOT NULL
-	,CONSTRAINT villeca_PK PRIMARY KEY (id_villeca)
+                        id_villeca Int  Auto_increment  NOT NULL ,
+                        villeca    Varchar (80) NOT NULL
+    ,CONSTRAINT villeca_PK PRIMARY KEY (id_villeca)
 )ENGINE=InnoDB;
 
 
@@ -123,9 +123,9 @@ CREATE TABLE villeca(
 #------------------------------------------------------------
 
 CREATE TABLE role_user(
-        id_role_user Int  Auto_increment  NOT NULL ,
-        role_user    Varchar (80) NOT NULL
-	,CONSTRAINT role_user_PK PRIMARY KEY (id_role_user)
+                          id_role_user Int  Auto_increment  NOT NULL ,
+                          role_user    Varchar (80) NOT NULL
+    ,CONSTRAINT role_user_PK PRIMARY KEY (id_role_user)
 )ENGINE=InnoDB;
 
 
@@ -134,16 +134,16 @@ CREATE TABLE role_user(
 #------------------------------------------------------------
 
 CREATE TABLE user(
-        id_user       Int  Auto_increment  NOT NULL ,
-        nom_user      Varchar (80) NOT NULL ,
-        prenom_user   Varchar (80) NOT NULL ,
-        password_user Varchar (80) NOT NULL ,
-        email_user    Varchar (256) NOT NULL ,
-        id_role_user  Int NOT NULL
-	,CONSTRAINT user_AK UNIQUE (email_user)
-	,CONSTRAINT user_PK PRIMARY KEY (id_user)
+                     id_user       Int  Auto_increment  NOT NULL ,
+                     nom_user      Varchar (80) NOT NULL ,
+                     prenom_user   Varchar (80) NOT NULL ,
+                     password_user Varchar (80) NOT NULL ,
+                     email_user    Varchar (256) NOT NULL ,
+                     id_role_user  Int NOT NULL
+    ,CONSTRAINT user_AK UNIQUE (email_user)
+    ,CONSTRAINT user_PK PRIMARY KEY (id_user)
 
-	,CONSTRAINT user_role_user_FK FOREIGN KEY (id_role_user) REFERENCES role_user(id_role_user)
+    ,CONSTRAINT user_role_user_FK FOREIGN KEY (id_role_user) REFERENCES role_user(id_role_user)
 )ENGINE=InnoDB;
 
 
@@ -152,9 +152,9 @@ CREATE TABLE user(
 #------------------------------------------------------------
 
 CREATE TABLE feuillage(
-        id_feuillage Int  Auto_increment  NOT NULL ,
-        feuillage    Varchar (80) NOT NULL
-	,CONSTRAINT feuillage_PK PRIMARY KEY (id_feuillage)
+                          id_feuillage Int  Auto_increment  NOT NULL ,
+                          feuillage    Varchar (80) NOT NULL
+    ,CONSTRAINT feuillage_PK PRIMARY KEY (id_feuillage)
 )ENGINE=InnoDB;
 
 
@@ -163,37 +163,38 @@ CREATE TABLE feuillage(
 #------------------------------------------------------------
 
 CREATE TABLE arbre(
-        id_arbre     Int  Auto_increment  NOT NULL ,
-        latitude     Double Precision NOT NULL ,
-        longitude    Double Precision NOT NULL ,
-        haut_tot     Int NOT NULL ,
-        haut_tronc   Int NOT NULL ,
-        tronc_diam   Int NOT NULL ,
-        revetement   Varchar (80) NOT NULL ,
-        age_estim    Int ,
-        clc_nbr_diag Int NOT NULL ,
-        remarquable  Varchar (80) NOT NULL ,
-        id_etat      Int NOT NULL ,
-        id_stadedev  Int NOT NULL ,
-        id_port      Int NOT NULL ,
-        id_pied      Int NOT NULL ,
-        id_situation Int NOT NULL ,
-        id_nomtech   Int NOT NULL ,
-        id_villeca   Int NOT NULL ,
-        id_user      Int ,
-        id_secteur   Int NOT NULL ,
-        id_feuillage Int NOT NULL
-	,CONSTRAINT arbre_PK PRIMARY KEY (id_arbre)
+                      id_arbre     Int  Auto_increment  NOT NULL ,
+                      latitude     Double Precision NOT NULL ,
+                      longitude    Double Precision NOT NULL ,
+                      haut_tot     Int NOT NULL ,
+                      haut_tronc   Int NOT NULL ,
+                      tronc_diam   Int NOT NULL ,
+                      revetement   Bool NOT NULL ,
+                      age_estim    Int ,
+                      prec_estim   Int NOT NULL ,
+                      clc_nbr_diag Int NOT NULL ,
+                      remarquable  Bool NOT NULL ,
+                      id_etat      Int NOT NULL ,
+                      id_stadedev  Int NOT NULL ,
+                      id_port      Int NOT NULL ,
+                      id_pied      Int NOT NULL ,
+                      id_situation Int NOT NULL ,
+                      id_nomtech   Int NOT NULL ,
+                      id_villeca   Int NOT NULL ,
+                      id_user      Int ,
+                      id_secteur   Int NOT NULL ,
+                      id_feuillage Int NOT NULL
+    ,CONSTRAINT arbre_PK PRIMARY KEY (id_arbre)
 
-	,CONSTRAINT arbre_etat_FK FOREIGN KEY (id_etat) REFERENCES etat(id_etat)
-	,CONSTRAINT arbre_stadedev0_FK FOREIGN KEY (id_stadedev) REFERENCES stadedev(id_stadedev)
-	,CONSTRAINT arbre_port1_FK FOREIGN KEY (id_port) REFERENCES port(id_port)
-	,CONSTRAINT arbre_pied2_FK FOREIGN KEY (id_pied) REFERENCES pied(id_pied)
-	,CONSTRAINT arbre_situation3_FK FOREIGN KEY (id_situation) REFERENCES situation(id_situation)
-	,CONSTRAINT arbre_nomtech4_FK FOREIGN KEY (id_nomtech) REFERENCES nomtech(id_nomtech)
-	,CONSTRAINT arbre_villeca5_FK FOREIGN KEY (id_villeca) REFERENCES villeca(id_villeca)
-	,CONSTRAINT arbre_user6_FK FOREIGN KEY (id_user) REFERENCES user(id_user)
-	,CONSTRAINT arbre_secteur7_FK FOREIGN KEY (id_secteur) REFERENCES secteur(id_secteur)
-	,CONSTRAINT arbre_feuillage8_FK FOREIGN KEY (id_feuillage) REFERENCES feuillage(id_feuillage)
+    ,CONSTRAINT arbre_etat_FK FOREIGN KEY (id_etat) REFERENCES etat(id_etat)
+    ,CONSTRAINT arbre_stadedev0_FK FOREIGN KEY (id_stadedev) REFERENCES stadedev(id_stadedev)
+    ,CONSTRAINT arbre_port1_FK FOREIGN KEY (id_port) REFERENCES port(id_port)
+    ,CONSTRAINT arbre_pied2_FK FOREIGN KEY (id_pied) REFERENCES pied(id_pied)
+    ,CONSTRAINT arbre_situation3_FK FOREIGN KEY (id_situation) REFERENCES situation(id_situation)
+    ,CONSTRAINT arbre_nomtech4_FK FOREIGN KEY (id_nomtech) REFERENCES nomtech(id_nomtech)
+    ,CONSTRAINT arbre_villeca5_FK FOREIGN KEY (id_villeca) REFERENCES villeca(id_villeca)
+    ,CONSTRAINT arbre_user6_FK FOREIGN KEY (id_user) REFERENCES user(id_user)
+    ,CONSTRAINT arbre_secteur7_FK FOREIGN KEY (id_secteur) REFERENCES secteur(id_secteur)
+    ,CONSTRAINT arbre_feuillage8_FK FOREIGN KEY (id_feuillage) REFERENCES feuillage(id_feuillage)
 )ENGINE=InnoDB;
 
