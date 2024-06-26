aaf33fd6289805ea587184ec3635cafceda08fd6
eltar14 <lebouant2@gmail.com>
2024-06-26 12:03:02 +0200

    Ville.php ajout methodes

--- client/php/Ville.php
+++ client/php/Ville.php
@@ -24,4 +24,50 @@ class Ville
     }
 
 
+    static function get_id_by_name($name)
+    {
+        $db = DB::connexion();
+        $request = "
+            SELECT villeca FROM villeca
+        WHERE LOWER(villeca) LIKE LOWER(:name)
+            ";
+
+        $statement = $db->prepare($request);
+        $statement->bindParam(':name', $name);
+
+        $statement->execute();
+        return $statement->fetch()[0];
+    }
+
+    static function add($name){
+        try
+        {
+            $db = DB::connexion();
+            $request = "
+        INSERT INTO villeca(villeca) VALUES(:name); 
+        ";
+
+            $statement = $db->prepare($request);
+            $statement->bindParam(':name', $name);
+
+            $statement->execute();
+            return $db->lastInsertId();
+
+        }
+        catch (PDOException $exception)
+        {
+            error_log('Request error: ' . $exception->getMessage());
+            return "error";
+        }
+    }
+
+    static function get_id_x_add_villeca($name)
+    {
+        $id = Ville::get_id_by_name($name);
+        if($id == NULL){
+            $id = Ville::add($name);
+        }
+        return $id;
+    }
+
 }
