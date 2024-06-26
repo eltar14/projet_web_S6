b1451d8a4fe8f39da786c0d85d3d424706f9dcb2
eltar14 <lebouant2@gmail.com>
2024-06-26 11:43:03 +0200

    Feuillage.php

--- client/php/Feuillage.php
+++ client/php/Feuillage.php
@@ -24,4 +24,53 @@ class Feuillage
     }
 
 
+
+    static function get_id_by_name($name)
+    {
+        $db = DB::connexion();
+        $request = "
+            SELECT feuillage FROM feuillage
+        WHERE LOWER(feuillage) LIKE LOWER(:name)
+            ";
+
+        $statement = $db->prepare($request);
+        $statement->bindParam(':name', $name);
+
+        $statement->execute();
+        return $statement->fetch()[0];
+    }
+    static function add($name){
+        try
+        {
+            $db = DB::connexion();
+            $request = "
+        INSERT INTO feuillage(feuillage) VALUES(:name); 
+        ";
+
+            $statement = $db->prepare($request);
+            $statement->bindParam(':name', $name);
+
+            $statement->execute();
+            return $db->lastInsertId();
+
+        }
+        catch (PDOException $exception)
+        {
+            error_log('Request error: ' . $exception->getMessage());
+            return "error";
+        }
+    }
+
+    static function get_id_x_add_feuillage($name)
+    {
+        $id = Nomtech::get_id_by_name($name);
+        if($id == NULL){
+            $id = Nomtech::add($name);
+        }
+        return $id;
+    }
+
+
+
+
 }
