b36d52134801c3d743a6565e875f42dc81cc4adc
eltar14 <lebouant2@gmail.com>
2024-06-26 11:43:36 +0200

    Port.php

--- client/php/Port.php
+++ client/php/Port.php
@@ -24,4 +24,51 @@ class Port
     }
 
 
+
+
+    static function get_id_by_name($name)
+    {
+        $db = DB::connexion();
+        $request = "
+            SELECT port FROM port
+        WHERE LOWER(port) LIKE LOWER(:name)
+            ";
+
+        $statement = $db->prepare($request);
+        $statement->bindParam(':name', $name);
+
+        $statement->execute();
+        return $statement->fetch()[0];
+    }
+
+    static function add_port($name){
+        try
+        {
+            $db = DB::connexion();
+            $request = "
+        INSERT INTO port(port) VALUES(:name); 
+        ";
+
+            $statement = $db->prepare($request);
+            $statement->bindParam(':name', $name);
+
+            $statement->execute();
+            return $db->lastInsertId();
+
+        }
+        catch (PDOException $exception)
+        {
+            error_log('Request error: ' . $exception->getMessage());
+            return "error";
+        }
+    }
+
+    static function get_id_x_add_port($name)
+    {
+        $id = Port::get_id_by_name($name);
+        if($id == NULL){
+            $id = Port::add_port($name);
+        }
+        return $id;
+    }
 }
