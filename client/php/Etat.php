0c3dec7c049ae11e602eda7e11fb6688f79b9a85
eltar14 <lebouant2@gmail.com>
2024-06-26 11:42:51 +0200

    Etat

--- client/php/Etat.php
+++ client/php/Etat.php
@@ -21,4 +21,49 @@ class Etat
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_COLUMN);
     }
+
+    static function get_id_by_name($name)
+    {
+        $db = DB::connexion();
+        $request = "
+            SELECT id_etat FROM etat
+        WHERE LOWER(arb_etat) LIKE LOWER(:name)
+            ";
+
+        $statement = $db->prepare($request);
+        $statement->bindParam(':name', $name);
+
+        $statement->execute();
+        return $statement->fetch()[0];
+    }
+
+    static function add_etat($etat)
+    {
+        try
+        {
+        $db = DB::connexion();
+        $request = "
+        INSERT INTO etat(arb_etat) VALUES(:etat); 
+        ";
+        $statement = $db->prepare($request);
+        $statement->bindParam(':etat', $etat);
+
+        $statement->execute();
+        return $db->lastInsertId();
+        }
+        catch (PDOException $exception)
+        {
+            error_log('Request error: ' . $exception->getMessage());
+            return "error";
+        }
+    }
+
+    static function get_id_x_add_etat($name){
+        $id = Etat::get_id_by_name($name);
+        if($id == NULL){
+            $id = Etat::add_etat($name); //
+        }
+        return $id;
+    }
+
 }
