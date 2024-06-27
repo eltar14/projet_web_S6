309e4ebd8fea953f06bc82fb3c64918f6f7fa292
TaMLeNok <t.lelievre718@gmail.com>
2024-06-27 17:04:28 +0200

    page d'accueil et page sympa

---
+++ client/views/cookies.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="fr" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>OEE LES ARBRES</title>
+    <link rel="icon" type="image/x-icon" href="/client/views/img/logo.png">
+    <!--    Our styles-->
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossorigin=""/>
+    <!-- JS Scripts -->
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+            crossorigin=""></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"
+            integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
+
+</head>
+<?=$navbar?>
+
+<body>
+
+<section id="intro" class="main_content">
+    <h1>
+        Recette de cookies
+    </h1>
+    <hr>
+    <h2>Voici la recette de nos cookies</h2>
+    <h3>Ingrédients :</h3>
+    <p> - 225 g de beurre doux, ramolli</p>
+    <p> - 200 g de sucre blanc </p>
+    <p> - 220 g de sucre brun</p>
+    <p> - 2 œufs</p>
+    <p> - 1 cuillère à café d'extrait de vanille</p>
+    <p> - 375 g de farine tout usage</p>
+    <p> - 1 cuillère à café de bicarbonate de soude</p>
+    <p> - 1/2 cuillère à café de levure chimique</p>
+    <p> - 1/2 cuillère à café de sel</p>
+    <p> - 350 g de pépites de chocolat</p>
+    <p> - 100 g de noix hachées (optionnel)</p>
+    <h3> Préparation du four :</h3>
+
+    <p> Préchauffez votre four à 180°C (350°F).</p>
+    <p>  Tapissez une plaque à pâtisserie de papier sulfurisé ou utilisez une plaque en silicone.</p>
+    <h3> Préparation de la pâte :</h3>
+
+    <p> Dans un grand bol, crémez le beurre ramolli avec le sucre blanc et le sucre brun jusqu'à ce que le mélange soit léger et mousseux.
+        Ajoutez les œufs un à un, en battant bien après chaque ajout. Incorporez l'extrait de vanille.
+        Incorporation des ingrédients secs :</p>
+
+    <p> Dans un autre bol, mélangez la farine, le bicarbonate de soude, la levure chimique et le sel.
+        Ajoutez progressivement le mélange de farine au mélange de beurre et de sucre, en mélangeant bien jusqu'à ce que tout soit bien incorporé.
+        Ajout des pépites de chocolat et des noix :</p>
+
+    <p> Incorporez les pépites de chocolat et les noix hachées (si vous en utilisez) à la pâte.
+        Formation des cookies :</p>
+
+    <p> À l'aide d'une cuillère à soupe ou d'une cuillère à glace, déposez des boules de pâte sur la plaque à pâtisserie préparée, en les espaçant de 5 cm environ.</p>
+    <h3> Cuisson :</h3>
+
+    <p> Faites cuire les cookies dans le four préchauffé pendant 10 à 12 minutes, ou jusqu'à ce que les bords soient dorés et que le centre soit encore légèrement mou.
+        Laissez les cookies refroidir sur la plaque pendant quelques minutes avant de les transférer sur une grille pour qu'ils refroidissent complètement.
+        Conseils : </p>
+    <p> Pour des cookies encore plus moelleux, remplacez 50 g de beurre par 50 g de fromage à la crème.
+        Ajoutez une pincée de sel de mer sur le dessus des cookies avant la cuisson pour un contraste sucré-salé délicieux.</p>
+</section>
+
+
+
+</body>
+<?=$footer?>
+
+<script rel="script" src="../js/ajax.js"></script>
+<script rel="script" src="../js/script.js"></script>
+<script rel="script" src="../js/main_map.js"></script>
+
+
+</html>
