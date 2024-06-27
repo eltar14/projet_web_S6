c4844b7ae61592bdbe16523263fe4fcfefc9c205
eltar14 <lebouant2@gmail.com>
2024-06-27 10:34:19 +0200

    main popups

--- client/js/main_map.js
+++ client/js/main_map.js
@@ -11,7 +11,15 @@ function main_map_callback(data){
             fillColor: '#f03',
             fillOpacity: 0.2,
             radius: (data[i]["tronc_diam"]/62.8)
-        }).addTo(map);
+        }).addTo(map).bindPopup(
+            'Hauteur totale : ' + data[i]["haut_tot"] +'m<br>'+
+            'Hauteur tronc : ' + data[i]["haut_tronc"] +'m<br>'+
+            'Remarquable : ' + (parseInt(data[i]["remarquable"])?'Oui':'Non') +'<br>'+
+            'Feuillage : ' + data[i]["feuillage"] +'<br>'+
+            'Stade dev : ' + data[i]["stadedev"] +'<br>'+
+            'Secteur : ' + data[i]["clc_secteur"] +'<br>'+
+            'Id arbre : ' + data[i]["id_arbre"] +'<br>'
+        );
 }
 }
 
